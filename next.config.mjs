6780c8aa733f346d986c935b5bcb95d8e046193d
harsh <prajapatiharshm119@gmail.com>
2025-10-05 14:26:22 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // 👈 ye line add ki gayi hai — ye 'out' folder banayegi
+
   eslint: {
     ignoreDuringBuilds: true,
   },
