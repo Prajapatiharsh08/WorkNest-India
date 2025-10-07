56af5faeed77467960e010444957154cf8be29dd
harsh <prajapatiharshm119@gmail.com>
2025-10-07 12:44:08 +0530

    updated

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,14 +1,3 @@
-// const express = require("express")
-// const router = express.Router()
-// const { sendContactEmail } = require("../controllers/contactController")
-
-// // POST route for contact form submission
-// router.post("/send", sendContactEmail)
-
-// module.exports = router
-
-
-
 const express = require("express");
 const { sendContactEmail } = require("../controllers/contactController");
 
